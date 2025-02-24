a6d31977bdb0cf59129e0dfdbb964abc7154cf05
Jisaula <juan.isaula@ficohsa.com>
2025-02-23 22:57:41 -0600

    Initial commit

---
+++ Configuracion_Git.R
@@ -0,0 +1,11 @@
+library(usethis)
+# Configurar el nombre de usuario en caso de no tenerlo 
+usethis::use_git_config(user.name = "Jisaula", user.email = "juan.isaula@ficohsa.com")
+use_git()
+use_github()
+
+# Generando Token en caso de no Tener 
+usethis::create_github_token()
+
+# Pegamos el token generado 
+gitcreds::gitcreds_set()
